644e46804067bca1a80848fb6c9f3d0426fa0bd9
Yarvin Hernandez <yarhumher@hotmail.com>
2020-04-30 21:39:28 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,7 @@ module Players
  class Computer < Player
    #
    def move(inputs)
-     input  = ["1","2","3","4",'5',"6","7","8","9"]
-    
-    p inputs.cells[0]
-    input[0]
+    inputs.cells
    end
  end
 end
